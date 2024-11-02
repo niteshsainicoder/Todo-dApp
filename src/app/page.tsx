805deea2cfd572f57e54c3e8594a920fc19091e6
niteshsainicoder <kumarsundratijara@gmail.com>
2024-11-02 13:19:24 +0530

    Project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,84 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from "react"
+import { connectWallet, addTodoOwner, addTodo, fetchTodos, deleteTodo, updateTodo } from "../lib/ether"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [settodo, setSettodo] = useState<string>("");
+  const [Alltodo, setAlltodo] = useState<string[]>();
+  const [val, setval] = useState('');
+  const getTodo = async () => {
+    setTimeout(async () => {
+      await fetchTodos().then((res) => {
+        setAlltodo(res);
+      }).catch((err) => {
+        console.log(err);
+      })
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    }, 1000)
+
+  }
+  const updateTodos = async (todoId: number, updatedTodo: string) => {
+    if (settodo !== '') {
+
+      updateTodo(todoId, updatedTodo).then(() => {
+        getTodo();
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+  const deleteTodos = async (todoId: number) => {
+    deleteTodo(todoId).then(() => {
+      getTodo();
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const addTodos = async (todo: string) => {
+    await addTodo(todo).then(() => {
+      getTodo();
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+  useEffect(() => {
+    connectWallet().then((res) => {
+      getTodo();
+      alert('trying to connect wallet');
+console.log(res);
+
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+
+
+  return (
+    <div className="w-full h-screen flex flex-col  sm:justify-start  caret-transparent gap-7 p-4 sm:p-12 items-center">
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+        <h1 className="text-xl select-none">{val !== '' ? val : 'Check you are registered as a Todo owner'}</h1>
+        <p onClick={async () => { setval(await addTodoOwner()) }} className="w-fit h-fit p-1 px-3 bg-gray-500 text-zinc-800 font-semibold hover:text-zinc-100 rounded-lg cursor-pointer hover:bg-gray-700"> click here!</p>
+      </div>
+      <div className=" flex gap-4 w-1/2 justify-center">
+        <input onChange={(e) => setSettodo(e.target.value)} type="text" name="todo" title="todo" placeholder="Add Todo" className=" w-full sm:w-2/3 p-1  border border-zinc-700 hover:border-zinc-500 outline-none px-2 bg-gray-700 rounded-lg" />
+        <button onClick={() => addTodos(settodo)} type="button" className="w-fit h-fit p-1 px-5 border border-zinc-700 hover:border-zinc-500  bg-gray-700 rounded-lg cursor-pointer hover:bg-gray-800">Add</button>
+      </div>
+      <div className="w-full sm:w-1/2 h-full flex border relative border-stone-500 p-2 rounded-lg  flex-col items-center justify-start gap-2">
+        <h1 className=" w-full text-center text-xl select-none text-gray-300 font-semibold mb-4 border-b-2 border-stone-600">Todo List</h1>
+        <p className="w-fit h-fit p-[2px] sm:text-sm px-3 top-1 sm:px-2 sm:top-[6px] right-4 flex absolute  bg-gray-500  font-semibold hover:text-zinc-100 rounded-lg cursor-pointer hover:bg-gray-700  text-green-300  justify-center items-center gap-2" onClick={getTodo}> <span className="rotate-12 ">↺</span> <span className="hidden sm:inline text-gray-800"> Refresh</span></p>
+        <div className="w-full h-full overflow-y-auto removescrollbar gap-1 flex flex-col scroll-smooth">
+          {Alltodo?.map((todo, key) =>
+          (<div key={todo} className="w-full h-10  flex justify-between items-center p-1 px-3 bg-gray-500 font-semibold rounded-md text-ellipsis">
+            <p className="w-full  text-zinc-700 hover:text-zinc-200 max-w-72 text-nowrap pr-7 p-1 overflow-hidden  text-ellipsis">{todo}</p>
+            <div className="flex gap-4">
+              <button type="button" className=" text-lg text-[#f13d30] hover:text-[#ff774d] outline-none" onClick={() => deleteTodos(key)}>X</button>
+              <button type="button" className="text-[#abf62d] hover:text-[#bdff4b] outline-none" onClick={() => { updateTodos(key, settodo) }}>Update</button>
+            </div></div>))
+          }
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </div>)
 }
---
+++ src/lib/ether.ts
@@ -0,0 +1,93 @@
+import "web3";
+import { ethers } from "ethers";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let contract: any;
+let provider;
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ethereum: any;
+  }
+}
+
+// Set up provider for Sepolia network
+//  provider = new ethers.JsonRpcProvider(
+//   "https://sepolia.infura.io/v3/4e05672f1a724e6dad2dfa60dedd4896"
+// );
+
+// Function to get contract instance for writing data (requires signer)
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      provider = new ethers.BrowserProvider(window.ethereum);
+
+      // Request access to the user's wallet (MetaMask)
+      await provider.send("eth_requestAccounts", []);
+
+      const signer = await provider.getSigner();
+      contract = new ethers.Contract(process.env.contractAddress!, process.env.ABI!, signer);
+
+      return contract;
+    } catch (error) {
+      console.log("Wallet connection error:", error);
+    }
+  } else {
+    console.error("Ethereum object not found. Please install MetaMask.");
+  }
+};
+
+export const addTodoOwner = async () => {
+  try {
+    // Attempt to register the user
+    if (!contract) {
+    return "Connect your wallet"
+    }
+    const tx = await contract.addTodoOwner();
+    await tx.wait(); // Wait for transaction to complete
+    return "Registered";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    // Check for the specific error message
+    if (error.message.includes("Already registered as a Todo owner")) {
+      return "Already Registered";
+    } else {
+      console.error("Error registering user:", error);
+      alert("An unexpected error occurred while trying to register.");
+      return "Error occured";
+    }
+  }
+};
+export const addTodo = async (todo: string) => {
+  const tx = await contract.addTodo(todo);
+  await tx.wait();
+  console.log(tx);
+  return tx;
+};
+
+export const fetchTodos = async () => {
+  if (!contract) {
+    return ["First connect wallet"];
+  }
+  const todos = await contract.getTodos();
+  return todos;
+};
+
+export const deleteTodo = async (todoId: number) => {
+  const tx = await contract.deleteTodo(todoId);
+  await tx.wait();
+  console.log(tx);
+  return tx;
+};
+export const updateTodo = async (todoId: number, updatedTodo: string) => {
+  if (!todoId || updatedTodo) {
+    console.log("please give updated todo");
+
+    return;
+  }
+  const tx = await contract.updateTodo(todoId, updatedTodo);
+  await tx.wait();
+  console.log(tx);
+  return tx;
+};
